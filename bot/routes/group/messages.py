c4d92bc8aec1e2d93f914461d8fa9cd451f2f95a
Kulunchick <nikolikrasovski@gmail.com>
2023-06-23 18:31:43 +0300

    FIX: Added users to group

--- bot/routes/group/messages.py
+++ bot/routes/group/messages.py
@@ -1,14 +1,24 @@
 from aiogram.types import Message
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
 
-from bot.models import User
+from bot.models import User, Group
+from bot.repositories.group import GroupRepository
 from bot.repositories.user import UserRepository
 
 
 async def messages(message: Message, session: AsyncSession):
-    user_repository = UserRepository(session)
-    user = await user_repository.get_by_id(message.from_user.id)
+    group_repository = GroupRepository(session)
+    group = await group_repository.get_by_id(message.chat.id, [selectinload(Group.users)])
+    if group is None:
+        group = Group(
+            id=message.chat.id,
+            title=message.chat.title
+        )
+        await group_repository.create(group)
 
+    user_repository = UserRepository(session)
+    user = await user_repository.get_by_id(message.from_user.id, [selectinload(User.groups)])
     if user is None:
         user = User(
             id=message.from_user.id,
@@ -17,3 +27,5 @@ async def messages(message: Message, session: AsyncSession):
         await user_repository.create(user)
     elif user.username != message.from_user.username:
         user.username = f"@{message.from_user.username}" or message.from_user.mention_html()
+
+    group.users.append(user)
