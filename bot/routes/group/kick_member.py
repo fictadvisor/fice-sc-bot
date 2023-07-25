53323833592445a1d16a1b780a54f95166bdca91
Kulunchick <nikolikrasovski@gmail.com>
2023-07-25 22:27:50 +0300

    FIX: delete users

--- bot/routes/group/kick_member.py
+++ bot/routes/group/kick_member.py
@@ -6,6 +6,6 @@ from bot.repositories.uow import UnitOfWork
 
 
 async def kick_member(event: ChatMemberUpdated, uow: UnitOfWork):
-    user = await uow.users.get_by_id(event.new_chat_member.user.id, [selectinload(User.groups)])
+    user = await uow.users.get_by_id(event.old_chat_member.user.id, [selectinload(User.groups)])
     if user is not None:
         await uow.users.delete(user.id)
